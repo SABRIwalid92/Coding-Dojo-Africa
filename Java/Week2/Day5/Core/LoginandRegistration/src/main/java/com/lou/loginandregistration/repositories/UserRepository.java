1da13a84eed3b78905fdca5daa07fbabfedd42a5
SABRIwalid92 <walid.sabri@esprit.tn>
2024-02-15 01:31:40 +0100

    Java Stack - W2D5 core

---
+++ Java/Week2/Day5/Core/LoginandRegistration/src/main/java/com/lou/loginandregistration/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.lou.loginandregistration.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lou.loginandregistration.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+	// for logging user
+	Optional<User>findByEmail(String email);
+	
+}
---
+++ Java/Week2/Day5/Core/LoginandRegistration/src/test/java/com/lou/loginandregistration/LoginandRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lou.loginandregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginandRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
